eec9ed870085123ed3273f80eb2747b269993efd
HendrikCromboom <hcromboom@gmail.com>
2020-10-29 10:11:03 +0100

    /products api working tru routing with a controller

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using asp_net_core_init.Models;
+using asp_net_core_init.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_core_init.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+
+            this.ProductService = productService;
+
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get() 
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
